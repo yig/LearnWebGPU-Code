010766d9a6451257a16c6c7aca1d09cb29b7f598
Elie Michel <elie.michel@exppad.com>
2024-01-31 08:58:46 +0100

    Add missing releases

--- main.cpp
+++ main.cpp
@@ -273,7 +273,9 @@ int main (int, char**) {
 		CommandBufferDescriptor cmdBufferDescriptor;
 		cmdBufferDescriptor.label = "Command buffer";
 		CommandBuffer command = encoder.finish(cmdBufferDescriptor);
+		encoder.release();
 		queue.submit(command);
+		command.release();
 
 		swapChain.present();
 
@@ -288,10 +290,13 @@ int main (int, char**) {
 	indexBuffer.destroy();
 	indexBuffer.release();
 
+	pipeline.release();
+	shaderModule.release();
 	swapChain.release();
 	device.release();
 	adapter.release();
 	instance.release();
+	surface.release();
 	glfwDestroyWindow(window);
 	glfwTerminate();
 
